2a5335f94cc1b2295b975a028b01299e1ee8dd16
ezza022 <alfirman.pahlepi@gmail.com>
2021-10-24 15:15:45 +0700

    update:fix color

--- server/index.js
+++ server/index.js
@@ -34,15 +34,13 @@ io.on("connect", (socket) => {
   socket.on("online", ({ name }, callback) => {
     const existingUser = users.some((user) => user.name === name);
 
-    const randomColor = "#" + (((1 << 24) * Math.random()) | 0).toString(16);
-
     if (existingUser) return callback("name is already taken");
 
     users.push({
       name,
       id: socket.id,
       room: { name: "", id: "", admin: "" },
-      color: randomColor,
+      color: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
     });
 
     io.emit("users", { users });
