3af47daa8cbc7ad24785316d19a9a75fb817978c
ezza022 <alfirman.pahlepi@gmail.com>
2021-10-30 14:42:36 +0700

    fix: random color

--- server/index.js
+++ server/index.js
@@ -24,6 +24,15 @@ const getUserById = (id) => users.find((user) => user.id === id);
 const getUsersInRoom = (roomId) =>
   users.filter((user) => user.room.id === roomId);
 
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 io.on("connect", (socket) => {
   socket.on("online", ({ name }, callback) => {
     const existingUser = users.some((user) => user.name === name);
@@ -34,7 +43,7 @@ io.on("connect", (socket) => {
       name,
       id: socket.id,
       room: { name: "", id: "", admin: "" },
-      color: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+      color: getRandomColor(),
       isReady: false,
     });
 
